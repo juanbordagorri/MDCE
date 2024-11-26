c696690376bfb4a686a135a466ffa5c757d677a9
juanbordagorri <juan.bordagorri@gmail.com>
2024-11-26 17:31:25 -0300

    termine de subir la carpeta de estudiante

---
+++ yo estudiante/ppt de clase/scripts/02_Oferta y demanda dinámica_expect adapt.R
@@ -0,0 +1,82 @@
+library('rootSolve')
+library('pracma')
+library('R.matlab')
+
+################# Modelo con expectativas adaptativas #####################
+# parámetros
+
+a = 10       # parámetro beta
+b = 5        # parámetro delta 
+Qd_bar = 1000 # parámetro alpha / autonomous quantity demanded
+Qs_bar = 250  # parámetro (-gamma) / autonomous quantity offered
+lambda = 0.5; # parámetro de expectativas 
+
+# maximum nb of periods and tolerance
+tmax = 50 
+tol = 0.00001 
+
+
+t = 1
+P = vector("numeric",2)
+Qs = vector("numeric",2)
+Qd = vector("numeric",2)
+
+P[t] = 25 # initial price
+Qs[t] = NaN # no value for Q
+Qd[t] = NaN
+
+t = 2
+P [t] = (1 - lambda * (( a + b) / a))  * P[t - 1]  + ( Qd_bar-Qs_bar ) * lambda / a
+Qd[t] = Qd_bar - a * P[t ]
+Qs[t] = Qd[t]
+
+
+while ( t < tmax & abs ( P[t] - P[t-1] ) > tol * P [t]) {
+  t = t + 1;
+  P [t] = (1 - lambda * (( a + b) / a))  * P[t - 1]  + ( Qd_bar-Qs_bar ) * lambda / a 
+  Qd[t] = Qd_bar - a * P[t]
+  Qs[t] = Qd[t]
+  }
+pmin=min(P[1:length(P)])
+pmax=max(P[1:length(P)])
+plot(seq(0,pmax,1),seq(0,pmax,1),xlim=c(pmin,pmax),ylim=c(pmin,pmax),type="l",xlab="P(t)",ylab="P(t+1)",main="Diagrama de fase")
+#phase diagram ( price )
+for (i in 2:length(P)){
+  points(P[i-1],P[i-1])
+  points(P[i-1],P[i])
+  segments(P[i-1],P[i-1],P[i-1],P[i],col=rainbow(i))
+  segments(P[i-1],P[i],P[i],P[i],col=rainbow(i))
+}
+
+ymin = min (P)*.95; 
+ymax = max (P)*1.05;
+yy = c(ymin, ymax)
+
+### plot 1
+plot (P,type="l",xlab="t") 
+
+### plot 2
+ymax
+Pt=seq(ymin , ymax,((ymax-ymin)/ (length(P)-1) ))
+plot(Qd_bar - a*Pt,Pt, type="l",ylab="Pt",xlab="Qt",xlim=c(200,800))
+lines(Qs_bar + b*Pt,Pt)
+
+for (i in 2:length(P)){
+  lines(Qs_bar + b*P[i],P[i],type="p")
+  lines(Qd_bar - a*P[i], P[i],type="p")
+  segments(Qs_bar + b*P[i-1],P[i-1],Qd_bar - a*P[i-1],P[i-1],col="red",lwd=2)
+  segments(Qd_bar - a*P[i-1],P[i-1],Qs_bar + b*P[i],P[i],col="green",lwd=2)
+}
+
+#### plot 3
+plot(seq(0,ymax,1),seq(0,ymax,1),xlim=c(ymin,ymax),ylim=c(ymin,ymax),type="l",xlab="P(t)",ylab="P(t+1)")
+#phase diagram ( price )
+for (i in 2:length(P)){
+  points(P[i-1],P[i-1])
+  points(P[i-1],P[i])
+  segments(P[i-1],P[i-1],P[i-1],P[i],col=rainbow(i))
+  segments(P[i-1],P[i],P[i],P[i],col=rainbow(i))
+}
+
+
+
